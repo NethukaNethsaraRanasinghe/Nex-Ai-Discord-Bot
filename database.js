8e95773763256ed6f71bbf6b923905b016eb8d53
Nethuka <158580923+NethukaNethsaraGithub@users.noreply.github.com>
2024-06-21 10:36:56 +0530

    Create database.js

---
+++ database.js
@@ -0,0 +1,11 @@
+const mysql = require('mysql');
+
+const pool = mysql.createPool({
+  connectionLimit: 10,
+  host: 'your_database_host',
+  user: 'your_database_username',
+  password: 'your_database_password',
+  database: 'your_database_name'
+});
+
+module.exports = pool;
